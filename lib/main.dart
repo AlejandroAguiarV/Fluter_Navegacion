f33623451058d50d3d3c9a8bf9a79ea0bf7bd25b
ALEJANDRO_AGUIAR_VAZQUEZ <a.20308051280992@cbtis128.edu.mx>
2023-05-11 22:28:08 +0000

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+//Importo todas las pantallas de mi proyecto
+import 'screens/primary.dart';
+import 'screens/secondary.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,35 +11,24 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      //Quitamos debug
+      debugShowCheckedModeBanner: false,
+      //Configurar tema
+      //La pantalla inicial
+      initialRoute: "/primary",
+      //Las rutas de pantalla del proyecto
+      routes: {
+        "/primary": (BuildContext context) => Primary(),
+        "/secondary": (BuildContext context) => Secondary(),
+      },
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          primarySwatch: Colors.purple,
+          accentColor: Colors.red,
+          //Configurar texto
+          textTheme: TextTheme(
+              bodyText2: TextStyle(color: Colors.pink, fontSize: 30))),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      //home: BotonFlotante(),
     );
   }
 }
